874d3bb9b691a142f5b4c0327a42f479132e18f2
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-09-02 19:27:30 -0400

    Update user.py

--- api/user.py
+++ api/user.py
@@ -3,16 +3,15 @@ from api.request import RequestData
 
 def format_player(uuid) -> dict:
     profile = {
-        'PlayerID': {
-            'PlayerName': 'Trmazi',
-            'InviteCnt': 1,
-            'ArcadePt': 100,
-            'FriendRequested': 0,
-            'UpdateDate': '9/1/2022',
-            'LoginBonusId': 1,
-            'LoginCount': 6,
-            'Login': True,
-        }
+        'PlayerID': '1',
+        'PlayerName': 'Trmazi',
+        'InviteCnt': 1,
+        'ArcadePt': 100,
+        'FriendRequested': 0,
+        'UpdateDate': '9/1/2022',
+        'LoginBonusId': 1,
+        'LoginCount': 69,
+        'Login': True,
     }
     return profile
 
