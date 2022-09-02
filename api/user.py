a5970c2b36da511170f431439fa52c9611252ba9
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-09-02 19:10:58 -0400

    Update user.py

--- api/user.py
+++ api/user.py
@@ -10,4 +10,18 @@ class get_player(Resource):
         if request['uuid'] == None:
             return {'ErrorCode': 'bad uuid!'}
 
-        print(request['uuid'])
+        return {}
+
+class new_player(Resource):
+    def post(self):
+        request = RequestData.get_request_data()
+        if request != None:
+            uuid = request['uuid']
+            name = request['name']
+
+            if uuid == None:
+                return {'ErrorCode': 'bad uuid!'}
+            if name == None:
+                return {'ErrorCode': 'bad name!'}
+
+        else: return {}, 200
