2cf21af6b5f5a989f965f3c23494456a1afff8f7
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-09-02 19:20:39 -0400

    Update user.py

--- api/user.py
+++ api/user.py
@@ -1,16 +1,32 @@
 from flask_restful import Resource
 from api.request import RequestData
 
+def format_player(uuid) -> dict:
+    profile = {
+        'PlayerID': '1',
+        'PlayerName': 'FUCK',
+        'InviteCnt': 100,
+        'ArcadePt': 1000,
+        'FriendRequested': 0,
+        'UpdateDate': '9/1/2022',
+        'LoginBonusId': 1,
+        'LoginCount': 69,
+        'Login': True,
+    }
+    return profile
+
 class get_player(Resource):
     def post(self):
         request = RequestData.get_request_data()
-        if request == None:
-            return {'ErrorCode': 'bad request!'}
-        
-        if request['uuid'] == None:
-            return {'ErrorCode': 'bad uuid!'}
+        if request != None:
+            uuid = request['uuid']
 
-        return {}
+            if uuid == None:
+                return {'ErrorCode': 'bad uuid!'}
+
+            return format_player(uuid)
+
+        else: return {'ErrorCode': 'Bad request!'}, 200
 
 class new_player(Resource):
     def post(self):
@@ -24,4 +40,6 @@ class new_player(Resource):
             if name == None:
                 return {'ErrorCode': 'bad name!'}
 
-        else: return {}, 200
+            return format_player(uuid)
+
+        else: return {'ErrorCode': 'Bad request!'}, 200
