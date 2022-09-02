88f4a8ce3cb67801cc571333badb53ad734afee2
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-09-02 17:04:26 -0400

    Update main.py

--- api/utils/main.py
+++ api/utils/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 from typing import Any, Dict
 import argparse
@@ -10,9 +10,19 @@ config: Dict[str, Any] = {}
 class Top(Resource):
     def get(self):
         return "Pop'n Rhythmin Server!"
+        
+class get_dl_file_list(Resource):
+    def post(self):
+        print(request.get_json())
+        return {'balls'}
+
+# Service statics
+uri_end = '/index.jsp'
+uri_start = '/apr/main/cgi'
 
 #add services
 api.add_resource(Top, '/')
+api.add_resource(get_dl_file_list, f'{uri_start}/get_dl_file_list{uri_end}')
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="3rd party server for Pop'n Rhythin.")
