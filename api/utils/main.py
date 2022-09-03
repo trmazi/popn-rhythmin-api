4f73da1c0163652bc16d487ebac56810d52344f5
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-09-02 21:02:28 -0400

    Update main.py

--- api/utils/main.py
+++ api/utils/main.py
@@ -4,6 +4,7 @@ import argparse
 
 from api.events import get_dl_file_list, get_event_info
 from api.user import get_player, new_player
+from api.network import search_master
 
 app = Flask(__name__)
 api = Api(app)
@@ -25,6 +26,7 @@ api.add_resource(get_dl_file_list, f'{uri_start}/get_dl_file_list{uri_end}')
 api.add_resource(get_event_info, f'{uri_start}/get_event_info{uri_end}')
 api.add_resource(get_player, f'{uri_start}/get_player{uri_end}')
 api.add_resource(new_player, f'{uri_start}/new_player{uri_end}')
+api.add_resource(search_master, f'{uri_start}/search_master{uri_end}')
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="3rd party server for Pop'n Rhythin.")
