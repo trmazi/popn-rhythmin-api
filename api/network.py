a65a1a0c6a3cb5b789bcb448ca97b5f7c95a3573
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-09-02 21:22:19 -0400

    Update network.py

--- api/network.py
+++ api/network.py
@@ -2,16 +2,28 @@ from flask_restful import Resource
 
 class search_master(Resource):
     def get(self):
-        response = {'GameCenterList': [
-            {
-                'ID': 0,
-                'Lat': 39.908987,
-                'Long': -86.065242,
-                'Name': 'Boss Battle Games',
-                'Open': '1',
-                'Model': [1, 1, 1]
-            }
-        ]}
+        response = {
+            'GameCenterList': [
+                {
+                    'ID': 0,
+                    'Lat': 39.908987,
+                    'Long': -86.065242,
+                    'Name': 'Boss Battle Games',
+                    'Open': '1',
+                    'Model': [1]
+                }
+            ],
+            'Version': '200',
+            'Info': 'Da Map.',
+            'Mark': [
+                {
+                   'Order': '1',
+                   'Model': '0',
+                   'Name': 'Boss Battle Games',
+                   'Image': 'https://shorturl.at/emruW'
+                }
+            ]
+        }
 
         print(response)
         return response
